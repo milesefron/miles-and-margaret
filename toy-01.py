2727e0b1582d178d68d6a7e52fcb4376507310ab
Miles Efron <mefron@gmail.com>
2023-10-03 12:33:16 -0400

    adds iteration over sentences

--- toy-01.py
+++ toy-01.py
@@ -1,6 +1,8 @@
 import os
 
 
+targets = ['disappointing', 'great', 'awesome']
+
 data_directory = './data'
 file_listing = os.listdir(data_directory)
 
@@ -8,5 +10,11 @@ for file in file_listing:
     if('.txt' in file):
         f = open(data_directory + '/' + file)
         text = f.read()
+
+        sentences = text.split('.')
+        for sentence in sentences:
+            for target in targets:
+                if target in sentence:
+                    print(target + ":: " + sentence)
+
         f.close()
-        print(text)
